93a8cf8c401c80df5fb39035769edec0dd0c10d7
NOT_SØ_BAD <komumusic5@gmail.com>
2025-12-10 19:58:55 -0300

    basic commit

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -4,7 +4,7 @@ import './modules/commands';
 import './modules/events';
 // src/server/src/index.ts
 
-import { logger } from '../../../shared/utils/logger';  // ← un nivel más arriba
+import { logger } from '../../../shared/utils/logger';  // 4 niveles arriba: src/server/src → src/shared
 
 mp.events.add('playerJoin', (player: PlayerMp) => {
   logger.info(`Jugador ${player.name} se unió.`);
